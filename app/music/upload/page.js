82e712ccd6691d7f9ad9a64f21581a9f4f04a9f6
Akarsh Rajput <akarshrajput@Akarshs-MacBook-Air.local>
2025-07-13 16:22:01 +0530

    commit

--- app/music/upload/page.js
+++ app/music/upload/page.js
@@ -9,8 +9,8 @@ const page = async () => {
   const hostname = process.env.HOSTNAME;
 
   return (
-    <div className="mt-4 flex justify-center">
-      <div className="w-full">
+    <div className="px-2 my-10 mt-24 flex justify-center">
+      <div className="w-[1200px]">
         {session.user.role === "admin" || session.user.role === "guide" ? (
           <UploadMusic
             session={session}

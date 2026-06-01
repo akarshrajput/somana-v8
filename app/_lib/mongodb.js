1c9719ba4dc66baba6f7250616520edfe650b597
Akarsh Rajput <akarshrajput@Akarshs-MacBook-Air.local>
2026-06-01 08:45:19 +0530

    fix: hardcode database connection string in connectMongoDB to bypass Vercel env parsing issues

--- app/_lib/mongodb.js
+++ app/_lib/mongodb.js
@@ -1,12 +1,14 @@
 // utils/connectMongoDB.js
 import mongoose from "mongoose";
 
+const DATABASE_URI = "mongodb+srv://akarshrajput01:sLXy7RF5O5WIYbAY@somanacluster0.ugwqix9.mongodb.net/?appName=somanaCluster0";
+
 const connectMongoDB = async () => {
   if (mongoose.connections[0].readyState) {
     return;
   }
   try {
-    await mongoose.connect(process.env.DATABASE, {
+    await mongoose.connect(DATABASE_URI, {
       serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds if connection fails
     });
     console.log("Database Connected Successfully!");
@@ -17,4 +19,5 @@ const connectMongoDB = async () => {
 };
 
 export default connectMongoDB;
+
 

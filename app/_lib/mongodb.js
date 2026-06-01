4d4c923746b9f73580e3896c76d4ee91f56af5ba
Akarsh Rajput <akarshrajput@Akarshs-MacBook-Air.local>
2026-06-01 08:36:14 +0530

    fix: throw mongoose connection error and set serverSelectionTimeoutMS to prevent infinite loading on db connection failure

--- app/_lib/mongodb.js
+++ app/_lib/mongodb.js
@@ -7,13 +7,14 @@ const connectMongoDB = async () => {
   }
   try {
     await mongoose.connect(process.env.DATABASE, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds if connection fails
     });
     console.log("Database Connected Successfully!");
   } catch (err) {
     console.error("Database Connection Error:", err);
+    throw err;
   }
 };
 
 export default connectMongoDB;
+
